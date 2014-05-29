03b8fed26288e4b180bbdb4b0128b59595c6c91a
Shane Carlson <designshac@gmail.com>
2014-05-29 02:32:35 -0600

    1) Started working on the dashboard layout, the main components are slated in, just need to finish it up tomorrow

--- app/views/includes/dashboard/header.blade.php
+++ app/views/includes/dashboard/header.blade.php
@@ -4,7 +4,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   {{HTML::style('bootstrap/css/bootstrap.css')}}
   {{HTML::style('css/flat-ui.css')}}
+  {{HTML::style('css/font-awesome.min.css')}}
   {{HTML::style('css/dashboard/style.css')}}
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,800,700,600|Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
   <link rel="shortcut icon" href="images/favicon.ico">
   <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
   <!--[if lt IE 9]>
--- app/views/pages/user/dashboard.blade.php
+++ app/views/pages/user/dashboard.blade.php
@@ -2,21 +2,59 @@
 @section('content')
 <div class="container">
    <div class="row">
-      <div class="col-sm-10 col-sm-offset-1">
-         <div class="row">
-            <div class="col-sm-12">
-               <h5>
-                  Dashboard
-
-                  @if(Auth::check())
-                  <a class="btn btn-lg pull-right btn-primary" href="{{ URL::route('logout') }}">Logout</a>
-                  @endif
-
-               </h5>
+      <div class="col-sm-12">
+         @if(Auth::check())
+         <a class="btn btn-lg pull-right btn-primary" href="{{ URL::route('logout') }}">Logout</a>
+         @endif
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-sm-12 app-frame">
+         <div class="row app-header">
+            <div class="col-sm-9">
+               <div class="user-name pull-left">
+                  <h5>{{$user->firstname}} {{$user->lastname}}</h5>
+               </div>
+               <div class="user-role pull-left">
+                  <h6>Admin for <strong>@foreach($organizations as $organization)
+                     {{$organization->name}}
+                     @endforeach</strong></h6>
+                  </div>
+               </div>
+               <div class="col-sm-3">
+                  <div class="search pull-right">
+                     <div class="input-group">
+                        <input type="text" class="form-control">
+                        <span class="input-group-btn">
+                          <button class="btn btn-default" type="button">
+                            <span class="glyphicon glyphicon-search"></span>
+                         </button>
+                      </span>
+                   </div>
+                </div>
+             </div>
+          </div>
+          <div class="row">
+            <div class="menu">
+               <div class="user-avatar" style="background-image: url({{ URL::asset($user->avatar); }});">
+               </div>
+               <div class="logo" style="background-image: url({{ URL::asset('images/league-together-logo.svg'); }})"></div>
+               <div class="container menu-container">
+                  <div class="row menu-item">
+                     <h5>Dashboard</h5>
+                  </div>
+                  <div class="row menu-item">
+                     <h5>
+                        <i class="fa fa-users"></i><br />Management
+                     </h5>
+                  </div>
+                  <div class="row menu-item">
+                     <h5>
+                        <i class="fa fa-bar-chart-o"></i><br />Accounting
+                     </h5>
+                  </div>
+               </div>
             </div>
-         </div>
-         <hr>
-         <div class="row">
             <div class="col-sm-12">
                @if(Session::has('message'))
                <div class="row">
@@ -34,7 +72,7 @@
                   <div class="col-sm-6">
                      <h6 class="media-heading">User Info</h6>
                      <div class="media">
-                           {{HTML::image($user->avatar, $user->firstname, array('class'=>'img-thumbnail pull-right','width'=>95));}}
+                        {{HTML::image($user->avatar, $user->firstname, array('class'=>'img-thumbnail pull-right','width'=>95));}}
                         <div class="media-body">
                            <p>
                               <small>
@@ -49,48 +87,45 @@
                      </div>
                   </div>
                </div>
-            </div>
-         </div>
-         <hr>
-         <div class="row">
-            <div class="col-sm-12">
                <div class="row">
-                  <div class="col-sm-6">
-                     <div class="panel panel-default">
-                        <div class="panel-heading">
-                           <h3 class="panel-title">My Organizations</h3>
-                        </div>
-                        <div class="panel-body">
-                           <div class="list-group">
-                              @foreach($organizations as $organization)
-                              <a class="list-group-item" href="{{URL::action('OrganizationController@show', array($organization->id))}}">
-                                 {{HTML::image($organization->logo, $organization->name, array('class'=>'img-thumbnail','width'=>55));}}
-                                 {{$organization->name}}
-                              </a>
-                              @endforeach
+                  <div class="col-sm-12">
+                     <div class="row">
+                        <div class="col-sm-6">
+                           <div class="panel panel-default">
+                              <div class="panel-heading">
+                                 <h3 class="panel-title">My Organizations</h3>
+                              </div>
+                              <div class="panel-body">
+                                 <div class="list-group">
+                                    @foreach($organizations as $organization)
+                                    <a class="list-group-item" href="{{URL::action('OrganizationController@show', array($organization->id))}}">
+                                       {{HTML::image($organization->logo, $organization->name, array('class'=>'img-thumbnail','width'=>55));}}
+                                       {{$organization->name}}
+                                    </a>
+                                    @endforeach
+                                 </div>
+                              </div>
                            </div>
+                           <br>
+                           <a class="btn btn-embossed btn-primary" href="{{ URL::route('dashboard.organization.create') }}">New Organization</a>
                         </div>
                      </div>
-                     <br>
-                     <a class="btn btn-embossed btn-primary" href="{{ URL::route('dashboard.organization.create') }}">New Organization</a>
                   </div>
                </div>
-            </div>
-         </div>
-         <hr>
-         <div class="row">
-            <div class="col-sm-12">
                <div class="row">
-                  <div class="col-sm-6">
-                     Affiliations
-                     <br>
-                     <br>
-                     <a class="btn btn-embossed btn-primary" href="{{ URL::route('dashboard.organization.create') }}">Join Organization</a>
+                  <div class="col-sm-12">
+                     <div class="row">
+                        <div class="col-sm-6">
+                           Affiliations
+                           <br>
+                           <br>
+                           <a class="btn btn-embossed btn-primary" href="{{ URL::route('dashboard.organization.create') }}">Join Organization</a>
+                        </div>
+                     </div>
                   </div>
                </div>
             </div>
          </div>
-         <hr>
       </div>
    </div>
 </div>
